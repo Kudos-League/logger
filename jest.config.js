7765cde52c3545953006e4080383598b2e13f2ee
Isabella <isabellafj97@gmail.com>
2023-08-31 21:27:54 -0500

    Make sure logger implements fastifybaselogger

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   globals: {
--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,9 @@
 import 'dotenv/config';
-import pino from 'pino';
+import pino, { LoggerOptions } from 'pino';
 import fetch from 'node-fetch';
+import { FastifyBaseLogger } from 'fastify';
 
-export function wrap(logger: pino.Logger) {
-  const { error, child } = logger;
-
-  function errorRearranger(...args: any) {
-    if (typeof args[0] === 'string' && args.length > 1) {
-      for (let i = 1; i < args.length; i++) {
-        const arg = args[i];
-        if (arg instanceof Error) {
-          const [err] = args.splice(i, 1);
-          args.unshift(err);
-        }
-      }
-    }
-    return error.apply(this, args);
-  }
-
-  function childModifier(...args: any) {
-    const c = child.apply(this, args);
-    c.error = errorRearranger;
-    (c as any).child = childModifier;
-    return c;
-  }
-
-  logger.error = errorRearranger.bind(logger);
-  (logger as any).child = childModifier.bind(logger);
-
-  return logger;
-}
-
-const logger = wrap(
-  pino({
-    level: process.env.LOG_LEVEL || 'debug',
-    transport:
-      process.env.ENV !== 'development'
-        ? undefined
-        : {
-            target: 'pino-pretty',
-          },
-    hooks: {
-      logMethod(inputArgs, method, level) {
-        if (level === 50 && (inputArgs[0] as unknown) instanceof Error) {
-          const err = inputArgs[0] as unknown as Error;
-          let args: string[] = [];
-
-          if (err.cause) {
-            args = [
-              `${err.stack}\nCaused by: ${err.cause[0]} ${err.cause[0].stack}`,
-            ];
-          } else {
-            args = [err.stack || ''];
-          }
-
-          return method.apply(this, args as any);
-        }
-
-        // Handles additional arguments being passed in
-        for (let i = 0; i < inputArgs.length; ++i) {
-          if (Array.isArray(inputArgs[i])) {
-            inputArgs[i] = inputArgs[i].join(' ');
-          }
-
-          if (i !== 0) {
-            inputArgs[0] += ` ${inputArgs[i]}`;
-          }
-        }
-
-        return method.apply(this, inputArgs);
-      },
-    },
-  })
-);
-
-export async function postErrorToDiscord(
-  error: Error,
-  url: string,
-  content?: string
-) {
+async function postErrorToDiscord(error: Error, url: string, content?: string) {
   try {
     await fetch(url, {
       method: 'POST',
@@ -97,54 +22,80 @@ export async function postErrorToDiscord(
       },
     });
   } catch (e: any) {
-    logger.error('Error with Discord webhook', e);
+    this.error('Error with Discord webhook', e);
   }
 }
 
-export default class Logger {
-  content?: string;
-  webhookURL?: string;
+export function WrapLogger<T extends new (...args: any[]) => any>(
+  constructor: T,
+  _?: any
+) {
+  return class extends constructor {
+    error(...args: any[]) {
+      if (typeof args[0] === 'string' && args.length > 1) {
+        for (let i = 1; i < args.length; i++) {
+          const arg = args[i];
+          if (arg instanceof Error) {
+            const [err] = args.splice(i, 1);
+            args.unshift(err);
+          }
+        }
+      }
+      super.error(...args);
+    }
+  };
+}
 
-  constructor({
-    content,
-    webhookURL,
-  }: { content?: string; webhookURL?: string } = {}) {
+@WrapLogger
+export default class Logger implements FastifyBaseLogger {
+  private pinoLogger: pino.Logger;
+  private content?: string;
+  private webhookURL?: string;
+  public level: string;
+
+  constructor(
+    opts?: LoggerOptions,
+    { content, webhookURL }: { content?: string; webhookURL?: string } = {}
+  ) {
+    this.pinoLogger = pino(opts);
     this.content = content;
     this.webhookURL = webhookURL;
+    this.level = process.env.LOG_LEVEL || 'info';
   }
-
-  static debug(...args: any) {
-    logger.debug(args);
-  }
-
-  static info(...args: any) {
-    logger.info(args);
-  }
-
-  static warn(...args: any) {
-    logger.warn(args);
-  }
-
-  static error(...args: any) {
-    logger.error(args);
-  }
-
   error(...args: any) {
-    logger.error(args);
+    this.pinoLogger.error.apply(null, args);
     if (this.webhookURL) {
-      postErrorToDiscord(args[0], this.webhookURL, this.content);
+      postErrorToDiscord(args[0], this.webhookURL, this.content).catch((e) => {
+        this.pinoLogger.error('Error posting to Discord', e);
+      });
     }
   }
 
+  info(...args: any) {
+    this.pinoLogger.info.apply(null, args);
+  }
+
+  warn(...args: any) {
+    this.pinoLogger.warn.apply(null, args);
+  }
+
+  debug(...args: any) {
+    this.pinoLogger.debug.apply(null, args);
+  }
+
   trace(...args: any) {
-    logger.trace(args);
+    this.pinoLogger.trace.apply(null, args);
   }
 
   fatal(...args: any) {
-    logger.fatal(args);
+    this.pinoLogger.fatal.apply(null, args);
   }
 
   child(...args: any) {
-    return logger.child(args);
+    return this.pinoLogger.child.apply(null, args);
+  }
+
+  silent(...args: any) {
+    return this.pinoLogger.silent.apply(null, args);
   }
 }
