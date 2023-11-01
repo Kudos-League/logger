a27c91c37fe4098f93add012a93bf60ac0c42195
Isabella <isabellafj97@gmail.com>
2023-10-31 20:52:54 -0500

    Fix publish hopefully

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	transform: {
+		'^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.test.json',
+		},
+	},
 };
